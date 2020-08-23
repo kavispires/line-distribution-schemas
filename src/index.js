02c6e40b8d11bc574416975b72e873fcaa38bb89
Fabiano 'Kavis' Pires <kavispires@gmail.com>
2020-08-23 14:26:35 -0700

    Fixed babel transpile with webpack setup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 // General Exports
 import Collection from './collection';
 import LD from './ld';
+import * as urns from './urns';
 
 // Schema Exports
+import Album from './album';
 import Artist from './artist';
 import Unit from './unit';
 import Member from './member';
 
-export { Artist, Collection, LD, Member, Unit };
+export { Album, Artist, Collection, LD, Member, Unit, urns };
